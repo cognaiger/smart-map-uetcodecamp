c022c6b1d513d26af1ce76a2b79d13308d8419f8
Phạm Gia Việt Anh <vietanhpg2003@gmail.com>
2023-07-29 11:58:43 +0700

    ok

--- client/src/pages/FloorLayout/E3F1.jsx
+++ client/src/pages/FloorLayout/E3F1.jsx
@@ -1,16 +1,38 @@
 import { ReactComponent as Door } from "../../img/door.svg";
 import { ReactComponent as Stair } from "../../img/stairs.svg";
+import { useState } from "react";
+import RoomModal from "../../components/RoomModal/RoomModal";
 const E3F1 = () => {
+
+
+    
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [roomTitle, setRoomTitle] = useState("");
+    const [descrip, setDescrip] = useState("");
+
+    const roomInfo = {
+
+    }
+
+    const handleClick = (room) => {
+        setIsModalOpen(true);
+        setRoomTitle(roomInfo[room].title);
+        setDescrip(roomInfo[room].description);
+    }
+
     return (
         <div>
             <svg width={1500} height={1000}>
-                <rect x = {250} y = {800} width={100} height={200} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {250} y = {800} width={100} height={200} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}}
+                onClick={() => handleClick("TQ")} />
                 <text x={270} y={905} color="black">Thủ Quỹ</text>
                 
-                <rect x = {30} y = {550} width={220} height={450} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {30} y = {550} width={220} height={450} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} 
+                onClick={() => handleClick("KHTC")}/>
                 <text x={80} y={805} color="black">P. Kế Hoạch Tài Chính</text>
 
-                <rect x = {30} y = {100} width={220} height={450} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {30} y = {100} width={220} height={450} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}}
+                onClick={() => handleClick("PH")} />
                 <text x={120} y={305} color="black">Phòng Họp</text>
                 <rect x = {250} y = {100} width={180} height={150} style={{ fill: "lightpink", stroke: "black", strokeWidth: 3}} />
                 <text x={360} y={150} color="black">WC</text>
@@ -20,37 +42,47 @@ const E3F1 = () => {
                 
                 <Stair x={440} y = {-130} width = {50}/>
 
-                <rect x = {500} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {500} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}}
+                onClick={() => handleClick("103")} />
                 <text x={510} y={205} color="black">103: TTĐBCL</text>
 
-                <rect x = {630} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {630} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}}
+                onClick={() => handleClick("104")} />
                 <text x={640} y={205} color="black">104: CTSV</text>
 
-                <rect x = {760} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {760} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}}
+                onClick={() => handleClick("105")} />
                 <text x={770} y={205} color="black">105: P. Đào Tạo</text>
 
-                <rect x = {890} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {890} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} 
+                onClick={() => handleClick("106")}/>
                 <text x={900} y={205} color="black">106: P. Đào Tạo</text>
 
-                <rect x = {1020} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {1020} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}}
+                onClick={() => handleClick("107")} />
                 <text x={1030} y={205} color="black">107: P.Đào Tạo</text>
 
-                <rect x = {1150} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {1150} y = {100} width={130} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}}
+                onClick={() => handleClick("108")} />
                 <text x={1160} y={205} color="black">108: P. HCQT</text>
 
-                <rect x = {1280} y = {100} width={200} height={350} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {1280} y = {100} width={200} height={350} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}}
+                onClick={() => handleClick("E38")} />
                 <text x={1290} y={205} color="black">E38: Khoa Cơ học</text>
 
-                <rect x = {1280} y = {450} width={200} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {1280} y = {450} width={200} height={250} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}}
+                onClick={() => handleClick("109")} />
                 <text x={1290} y={505} color="black">109: Phòng Giáo Sư</text>
 
-                <rect x = {1280} y = {700} width={200} height={300} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}} />
+                <rect x = {1280} y = {700} width={200} height={300} style={{ fill: "lightgray", stroke: "black", strokeWidth: 3}}
+                onClick={() => handleClick("110")} />
                 <text x={1290} y={755} color="black">110: Trưởng Phòng HCQT</text>
 
                 <Door x={700} y={800} width={100} />
             
             
             </svg>
+            {isModalOpen && <RoomModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} title={roomTitle} descrip={descrip} />}
         </div>
     )
 }
